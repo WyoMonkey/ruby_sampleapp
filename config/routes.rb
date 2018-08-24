a6415cdc4bda8ee9a8d1112e3a6bb753d798d1ca
Lawrence Beck <sonny1388@msn.com>
2018-08-23 20:55:29 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
- root 'static_pages#home'
+  root 'static_pages#home'
 end
